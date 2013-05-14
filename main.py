0c255730036d084d239ee42760d482bb537d8cb8
Alexey Minakov <sintez.pm@gmail.com>
2013-05-14 20:25:33 +0400

    fixed proxy settings

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
-
 
 @app.route('/')
 def hello_world():
     return 'Hello World!'
 
+app.wsgi_app = ProxyFix(app.wsgi_app)
 
 if __name__ == '__main__':
     app.run()
